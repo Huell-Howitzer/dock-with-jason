50ebf1bed61d4b5d9204e2176f3a724fd1c106e9
Ryan M Howell <rmhowell@protonmail.com>
2022-12-28 11:06:12 -0800

    :rainbow: print

--- src/test.py
+++ src/test.py
@@ -1,1 +1,23 @@
-print("Jason... We have successfully docked with one-another.")
+from colorama import Fore, Back
+import random
+
+
+def random_foreground( ) :
+    return random.choice( [
+        Fore.MAGENTA,
+        Fore.RED,
+        Fore.BLACK
+    ] )
+
+
+def random_background( ) :
+    return random.choice( [
+        Back.WHITE,
+    ] )
+
+
+colors = dict( Fore.__dict__.items( ) )
+colored_text = 'Jason, We have Successfully docked with one another!'
+for character in colored_text :
+    print( random_foreground( ) + random_background( ) + character, end = '' )
+    # print(colors[random.choice(list(colors.keys()))] + character, end='')
